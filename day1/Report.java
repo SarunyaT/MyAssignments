119af0524793904d9d045d1bff8b8375b4256904
SarunyaT <149089318+SarunyaT@users.noreply.github.com>
2023-10-30 21:40:38 +0530

    Add files via upload

---
+++ day1/Report.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Report {
+	public static void main(String args[])
+	{
+		Student s1=new Student();
+		System.out.println("The student name is " + s1.stuName);
+		System.out.println("Student Roll Number is  " + s1.rollNo);
+		System.out.println("The College Name is  " + s1.collegeName);
+		System.out.println("Mark Scored " + s1.markScored);
+		System.out.println("The cgpa is " + s1.cgpa);
+	}
+
+}
---
+++ day1/Student.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Student {
+	String stuName="Sarunya";
+	int rollNo=45;
+	String collegeName="PSG";
+	int markScored=81;
+	float cgpa=7.45f;
+	
+
+}
